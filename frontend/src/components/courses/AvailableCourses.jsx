ee5e08ddf97afccb1d96469db2e870cbb8fd40f6
Nicky Chen <nickychen2022@gmail.com>
2025-04-05 21:41:02 -0400

    merged witih main

--- frontend/src/components/courses/AvailableCourses.jsx
+++ frontend/src/components/courses/AvailableCourses.jsx
@@ -3,31 +3,13 @@
 import CourseList from "./CourseList";
 import CourseItem from "./CourseItem";
 
-export const AvailableCourses = ({
-	// searchAvailable,
-	// setSearchAvailable,
-	courses,
-	getCourse,
-	isHorizontal = true
-	// excludedCourseIds
-}) => {
-	// const filteredCourses = filteredCourses
-	// 	? filteredCourses.filter(course => !excludedCourseIds.includes(course.course_id))
-	// 	: [];
-export const AvailableCourses = ({ courses, getCourse, limit }) => {
+export const AvailableCourses = ({ courses, getCourse, isHorizontal = true, limit }) => {
 	return (
-		<div
-			className={`non-draggable h-full border border-gray-200 rounded-md p-2.5 flex ${
-				isHorizontal ? "flex-row" : "flex-col"
-			} gap-3 overflow-x-auto overflow-y-hidden`}>
-			<CourseList
-				courses={courses}
-				getCourse={getCourse}
-				CourseItemComponent={CourseItem}
-			/>
 		<div className="p-2">
 			<div
-				className={`non-draggable border border-gray-200 rounded-md flex flex-row gap-3 overflow-x-auto overflow-y-hidden h-[200px] p-2`}>
+				className={`non-draggable border border-gray-200 rounded-md flex flex-row ${
+					isHorizontal ? "flex-row" : "flex-col"
+				} gap-3 overflow-x-auto overflow-y-hidden h-[200px] p-2`}>
 				<CourseList
 					courses={courses}
 					getCourse={getCourse}
