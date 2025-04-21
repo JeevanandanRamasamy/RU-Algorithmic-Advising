e28dbd3c1780c0cd9944e7dc0b0daae129f467c7
Johnathan Chan <johnathan.chan@rutgers.edu>
2025-04-20 23:51:21 -0400

    Removed duplicate imports

--- backend/test/integration/test_course_records_route.py
+++ backend/test/integration/test_course_records_route.py
@@ -1,7 +1,6 @@
 from app import create_app
 from backend.models.student_details import StudentDetails
 import pytest
-from app import create_app
 from flask_jwt_extended import create_access_token
 
 
